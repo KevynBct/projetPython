050a2ae64ae74278c08d069b8ca2b0a463b1ee44
Ylies AIT IHADDADENE <E14B310K@U-INF-J-E117-19.iut-nantes.univ-nantes.prive>
2016-04-26 17:20:19 +0200

    26/04/2016 17:20

--- bdd.py
+++ bdd.py
@@ -9,26 +9,33 @@ import csv
 #Connexion BDD
 conn = sqlite3.connect('base.db')
 
+# On vide la table 
+cursor = conn.cursor()
+cursor.execute("DROP table installation")
+cursor.execute("DROP table activite")
+cursor.execute("DROP table equipement")
+cursor.execute("DROP table equipement_activite")
+
 #Creation des tables
     
 #Table installation
-cursor = conn.cursor()
+#cursor = conn.cursor()
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS installation(
      numero_instal INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
      nom VARCHAR, 
+     numero INTEGER,
      adresse VARCHAR, 
      code_postal VARCHAR,
      ville VARCHAR,
-     latitude decimal,
-     longitude decimal
+     acces_pmr VARCHAR
 )
 """)
 conn.commit()
 
 
 #Table equipement
-cursor = conn.cursor()
+#cursor = conn.cursor()
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS equipement(
      numero_equipement INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
@@ -39,7 +46,7 @@ CREATE TABLE IF NOT EXISTS equipement(
 conn.commit()
 
 #Table equipement_activite
-cursor = conn.cursor()
+#cursor = conn.cursor()
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS equipement_activite(
      numero_equipement,
@@ -52,16 +59,21 @@ CREATE TABLE IF NOT EXISTS equipement_activite(
 conn.commit()
 
 #Table activite
-cursor = conn.cursor()
+#cursor = conn.cursor()
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS activite(
-     numero_activite INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-     nom VARCHAR
-)
+     id_activite INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, 
+     numero_activite INTEGER,
+     nom VARCHAR,
+     numero_equipement INTEGER, 
+     FOREIGN KEY(numero_equipement) REFERENCES equipement(numero_equipement)
+ )
 """)
 conn.commit()
 
-#Insertion des données dans la table à partir du fichier equipement.csv
+
+
+#Insertion data from equipement.csv
 with open('csv/equipements.csv', 'r') as equip:
     reader = csv.reader(equip)
 
@@ -72,14 +84,37 @@ with open('csv/equipements.csv', 'r') as equip:
             first = False
             continue
         else:
-            cursor.execute("INSERT INTO equipement(numero_equipement, nom, numero_installation) VALUES(?, ?, ?)", (row[4],row[5],row[2]))
-
+            cursor.execute("INSERT INTO equipement(numero_equipement, nom, numero_installation) VALUES (?, ?, ?)", (row[4],row[5],row[2]))
 conn.commit()
 
 
+#Insertion data from installations.csv
+with open('csv/installations.csv', 'r') as instal:
+    reader = csv.reader(instal)
 
+    #Flag pour eviter la lecture de la premiere ligne 
+    first = True
+    for row in reader:
+        if first:
+            first = False
+            continue
+        else:
+            cursor.execute("INSERT INTO installation(numero_instal, nom, adresse, code_postal, ville, acces_pmr) VALUES (?, ?, ?, ?, ?, ?)", (row[1],row[0],row[7],row[4],row[2], row[12]))
+conn.commit()
 
+#Insertion data from activite.csv
+with open('csv/activites.csv', 'r') as act:
+    reader = csv.reader(act)
 
+    #Flag pour eviter la lecture de la premiere ligne 
+    first = True
+    for row in reader:
+        if first:
+            first = False
+            continue
+        else:
+            cursor.execute("INSERT INTO activite(numero_activite, nom, numero_equipement) VALUES (?, ?, ?)", (row[4], row[5], row[2]))
+conn.commit()
 
 #Fermeture BDD
 conn.close()
--- test.py
+++ test.py
@@ -11,14 +11,8 @@ conn = sqlite3.connect('base.db')
 #Table installation
 cursor = conn.cursor()
 
-<<<<<<< HEAD
 for row in cursor.execute("""
-	select * from equipement
+	select * from activite
 	"""):
 	print(row)
 
-=======
-for row in cursor.execute("""select * from equipement"""):
-	
-print(row)
->>>>>>> 4a6a57f4611cda5756002e93182e8b7d57fa737d
